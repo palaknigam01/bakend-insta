a0099667542c44472a71c859cebf58c4fb339457
palak <palaknigam03@gmail.com>
2022-09-13 04:35:28 -0700

    sign-api

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,22 +1,49 @@
-const User = require('../model/user.model');
-const jwt = require('jsonwebtoken');
-const {validationResult } = require('express-validator');
-const { request, response } = require('express');
-exports.signup = (request,response)=>{
-    const errors = validationResult(request);
-    if(!errors.isEmpty()){
-        return response.status(403).json({errors:errors.array()})
-    }
-    let username = request.body.username;
-    let userEmail = request.body.email;
-    let userpassword = request.body.password;
+const User = require("../model/user.model");
+const jwt = require("jsonwebtoken");
+const { validationResult } = require("express-validator");
+const { request, response } = require("express");
+exports.signup = (request, response) => {
+  const errors = validationResult(request);
+  if (!errors.isEmpty()) {
+    return response.status(403).json({ errors: errors.array() });
+  }
+  let username = request.body.username;
+  let userEmail = request.body.email;
+  let userpassword = request.body.password;
 
-    User.create({username:username,email:userEmail,password:userpassword})
-    .then((result)=>{
-        console.log(result);
-        return response.status(200).json(result)
-    }).catch((err)=>{
-        console.log(err);
-        return response.status(500).json({errors:"server err..."})
+  User.create({ username: username, email: userEmail, password: userpassword })
+    .then((result) => {
+      console.log(result);
+      return response.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      return response.status(500).json({ errors: "server err..." });
     });
+};
+
+exports.signin = (request, response) => {
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+        return response.status(403).json({ errors: errors.array() })
+    }
+    let userEmail = request.body.email;
+    let userPasword = request.body.password;
+
+    User.findOne({ email: userEmail, password: userPasword })
+        .then(result => {
+            // console.log(result);
+            if (result) {
+                let payload = { subject: result._id };
+                let token = jwt.sign(payload, 'jskjkdjfjfkfj');
+                return response.status(201).json({
+                    result:result,
+                    token:token
+                });
+            }
+        }).catch(err => {
+            console.log(err);
+            return response.status(500).json({errors:'server err...'});
+
+        });
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,5 +4,6 @@ const userController = require("../controller/user.controller");
 const { body } = require("express-validator");
 
 router.post("/signup", body("email").isEmail(), userController.signup);
+router.post("/signin",body("email").isEmail(),userController.signin);
 
 module.exports = router;
